1c6656b0fa5b5b97b913d475aaec18c7234e606c
Felix Musmann <felix@musmann.io>
2018-07-04 14:53:08 +0200

    Add columns to output

--- memory_consumption.py
+++ memory_consumption.py
@@ -9,22 +9,28 @@ with open('benchmark/config.json', 'r') as f:
     config = json.load(f)
 
 # memory consumption for each query
-print("Memory consumption per query")
+print("MEMORY CONSUMPTION PER QUERY")
 for q in queries:
-    print("Query", q._nr, sum([column.size() for column in q._columns]))
+    print("\nQuery", q._nr)
+    print("Size:   ", sum([column.size() for column in q._columns]))
+    print("Columns:", [column._name for column in q._columns])
 
 # full replication
 full_sum = 0
 for t in tables:
     full_sum += sum([column.size() for column in t._columns])
-print("\nFull replication", full_sum)
+print("\n\nFULL REPLICATION")
+print("Size:", full_sum)
 
 # partial replication
-print("\nPartial replication")
+print("\n\nPARTIAL REPLICATION")
 for idx, serverConf in enumerate(config['connections']):
     node_columns = set()
-    node_sum = 0
     for q in queries:
         if q._nr in serverConf['queries']:
             node_columns.update(q._columns)
-    print(serverConf['host'], sum([column.size() for column in node_columns]))
+    node_sum = sum([column.size() for column in node_columns])
+    print()
+    print(serverConf['host'])
+    print("Size:      ", node_sum)
+    print("Percentage:", round(node_sum/full_sum*100, 1))
